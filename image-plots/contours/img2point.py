28d6624a1a16e60a630f07a098d61f64005a62bc
astoria-d <astoria-d@fujibon-inc.com>
2024-12-24 01:42:38 +0000

    update img2point

--- image-plots/contours/img2point.py
+++ image-plots/contours/img2point.py
@@ -26,11 +26,18 @@ def main():
     imgray = cv.cvtColor(input_img, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(imgray, 127, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    print(contours)
+#    print(contours)
+#    print(type(contours))
+#    print(type(contours[0]))
+#    print(type(contours[0][0]))
+#    print(type(contours[0][0][0]))
+#    print(type(contours[0][0][0][0]))
 
     # Create black empty images
-    W = 800
-    size = W, W, 3
+    height, width, channels = input_img.shape
+#    print(str(channels))
+    # 3 is color dimension (b, g, r)
+    size = height, width, 3
     bg_img = np.zeros(size, dtype=np.uint8)
 #    print(bg_img)
 
@@ -43,6 +50,23 @@ def main():
     cv.imshow("test", bg_img)
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+    out_fname = img_fname + '-plot'
+    if (os.path.exists(out_fname)):
+        print("[" + out_fname + "] already exists. removing it.")
+        os.remove(out_fname)
+
+    out_file = open(out_fname, "w")
+
+    printed = False
+    for c1 in contours:
+        if printed:
+            out_file.write(f'\n')
+        for c2 in c1:
+            for c3 in c2:
+                out_file.write(f'{c3[0]} {c3[1]}\n')
+        printed = True
+    out_file.close()
 
 if __name__ == "__main__":
     main()
