f48b3d16dc6fc5f4258f9aadbb92cf484dd270c4
astoria-d <astoria-d@fujibon-inc.com>
2025-01-05 20:58:54 +0000

    add frame delimiter bit

--- audio/modulation/encoder/ModulationCore.py
+++ audio/modulation/encoder/ModulationCore.py
@@ -4,6 +4,7 @@ import math
 STD_AMP = 1000
 BIT0_FREQ = 1000
 BIT1_FREQ = 2000
+FRAMER_FREQ = 4000
 SYM_PERIOD = 0.01
 
 
@@ -23,6 +24,7 @@ def create_sin_wave(data_period: float, sample_rate: int, amp: int, freq: int) -
 def encod_data(data: [], num_channel: int, sample_rate: int) -> [[]]:
     bit0_wave = create_sin_wave(SYM_PERIOD, sample_rate, STD_AMP, BIT0_FREQ)
     bit1_wave = create_sin_wave(SYM_PERIOD, sample_rate, STD_AMP, BIT1_FREQ)
+    framer_wave = create_sin_wave(SYM_PERIOD, sample_rate, STD_AMP, FRAMER_FREQ)
 
 #    print(bit0_wave)
 #    print(bit1_wave)
@@ -30,6 +32,8 @@ def encod_data(data: [], num_channel: int, sample_rate: int) -> [[]]:
     encoded_data = [[]] * num_channel
 
     for dt in data:
+        for ch in range(num_channel):
+            encoded_data[ch].extend(framer_wave)
         for i in range(8):
             b0 = dt & 0b1
             print(b0)
